e5822848f3c21d8d7abcc472b1f38f7194fec9a6
Alek Novkovski <alek@greatcode2.com>
2023-05-13 17:24:01 +0200

    Send back a quote obj on quotes/random route

--- working/server.js
+++ working/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const {quotes} = require('./data');
+const {getRandomElement} = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res) => {
+    res.json(
+        {
+            quote: {
+                quote: 'We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.',
+                person: 'Ellen Ullman'
+            }
+        }
+    )
+})
+
+
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}. . .`);
 })
+
